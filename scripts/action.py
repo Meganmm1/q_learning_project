bcaed3c936cf8b820a15d173508664f4c2872a8f
emilia <emilialim01@gmail.com>
2022-05-06 16:21:36 +0000

    choose action

--- scripts/action.py
+++ scripts/action.py
@@ -46,28 +46,65 @@ class Action(object):
 
         # cmd_vel publisher 
         self.pub_twist = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
-        def image_callback(self, msg):
 
+        self.action = -1
+        self.action_complete = False
+
+
+    def image_callback(self,msg)
         # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    
-        if self.actions.colors == "pink":
-            lower_pink = numpy.array([164, 63, 159])
-            upper_pink = numpy.array([164, 192, 255])
-        elif self.actions.colors == "green"
-            lower_green = numpy.array([60, 63, 192])
-            upper_green = numpy.array([60, 192, 255])
-        else self.actions.colors == "blue"
-            lower_blue = numpy.array([90, 63, 255])
-            upper_blue = numpy.array([104, 255, 255])
+        if self.action != -1:
+            action = (self.actions[self.action]) #get corresponding action for number
+            color = action['object']
+            ar_tag = action['tag']
+            #i think we would only need one lower and upper bound as we will only be 
+            #be looking at one color at a time
+            if color == "pink":
+                lower_pink = numpy.array([164, 63, 159])
+                upper_pink = numpy.array([164, 192, 255])
+            elif color == "green"
+                lower_green = numpy.array([60, 63, 192])
+                upper_green = numpy.array([60, 192, 255])
+            else color == "blue"
+                lower_blue = numpy.array([90, 63, 255])
+                upper_blue = numpy.array([104, 255, 255])
 
-        # this erases all pixels that aren't pink,green,blue
-        mask0 = cv2.inRange(hsv, lower_pink, upper_pink)             
-        mask1 = cv2.inRange(hsv, lower_green, upper_green)
-        mask2 = cv2.inRange(hsv, lower_blue, upper_blue)
+            # this erases all pixels that aren't pink,green,blue
+            mask0 = cv2.inRange(hsv, lower_pink, upper_pink)             
+            mask1 = cv2.inRange(hsv, lower_green, upper_green)
+            mask2 = cv2.inRange(hsv, lower_blue, upper_blue)
 
+            #have robot turn towards correct colored object
+            #have robot get to proper distance to colored object
+            #have robot pickup object
+            #have robot turn towards correct ar tag
+            #have robot go towards ar tag
+            #have robot place object
+            self.action_complete = True 
 
+    def choose_actions(self):
+        matrix = np.loadtxt(open("q_matrix.csv", "rb"), delimiter=",", skiprows=1)
+        
+        curr_state = 0
+        for i in range(3):
+            action = 0
+            max_q = 0
+            for i in range(9):
+                if matrix[curr_state][i] > max_q:
+                    action = i
+                    max_q = matrix[curr_state][i]
+            self.action = action #set action to maximum
+            self.move_to_color()
+            self.action_complete = False
+            while not self.action_complete:  #wait for action to be completed
+                var = 1
+            next_state = 0
+            while self.action_matrix[curr_state][next_state] != action and next_state < 64:
+                next_state += 1
+            curr_state = next_state
+        
 
 
 
